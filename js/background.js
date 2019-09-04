08736efc7daee37abd13fa477b0fecc28127fc2e
Higemura <higemura.mustache@gmail.com>
2019-09-05 03:52:42 +0900

    background js追加

--- js/background.js
+++ js/background.js
@@ -7,7 +7,17 @@ const filter = {
 }
   
 function logOnBefore(details) {
-	console.log("onBeforeNavigate to:::: " + details.url);
+	console.log("onBeforeNavigate to: " + details);
+	if (details.frameId !== 0) return;
+
+	const element = document.createElement('div');
+	element.innerHTML = "BeforeBegin";
+
+
+	document.body.appendChild(element);
+	console.log(element);
+	alert(document.body);
 }
   
+// chrome.webNavigation.onBeforeNavigate.addListener(logOnBefore, filter);
 chrome.webNavigation.onCompleted.addListener(logOnBefore, filter);
